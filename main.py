7a439b94497281b7d7fb0dede54cfa9c439cc8da
dave-progs <ottadavi@fel.cvut.cz>
2020-12-08 09:20:04 +0100

    První testovací commit.

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 >>> print_hi("Artem")
 "Hi, Artem"
 """
-# This is a sample Python script.
-
+# WORKS????
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
