3eb14c7e11508ea535f2cc4dd8264b442ad40952
Artem Bryksa <artem.bryksa@seznam.cz>
2020-12-24 22:20:57 +0100

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,35 @@
+"""
+    Author: David Otta
+    Created for: B4B33RPH
+    Created on 26. 10. 2020
+    Last change on 02. 11. 2020
+"""
+
+SPAM_TAG = "SPAM"
+HAM_TAG = "OK"
+
+def read_classification_from_file(file_name):
+    """
+        Filename contains 'mail_file_name OK/SPAM\n'
+        return: dict with 'mail_file_name': OK/SPAM
+    """
+    _dict = {}
+    with open(file_name, 'r', encoding='utf-8') as fr:
+        while fr:
+            line = fr.readline()
+            if line == "":
+                break
+            line_data = line.rstrip().split()
+            _dict[line_data[0]] = line_data[1]
+    return _dict
+
+
+def html_clean(word):
+    return str(word).replace("<br>", "").replace("</br>", "").strip('<').strip('>')
+
+
+def is_empty(any_structure):
+    if any_structure:
+        return False
+    else:
+        return True
